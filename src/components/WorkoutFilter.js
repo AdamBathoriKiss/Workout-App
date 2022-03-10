f7aa3b93750a3a505bde7c45ca4c0e6e01d7beb5
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-03-10 20:33:39 +0100

    Bugfix3

--- src/components/WorkoutFilter.js
+++ src/components/WorkoutFilter.js
@@ -4,18 +4,35 @@ import { useState } from 'react';
 
 function WorkoutFilter(props) {
   const [workoutname, setWorkoutName] = useState('');
+  const [workouts, setWorkouts] = useState([]);
+
     console.log(props);
   const handleSelect = e => {
     setWorkoutName(e.target.value);
     props.WorkoutFilter(e.target.value);
     console.log('selected', e.target.value);
+
+    const getAllWorkouts = () => {
+        axios
+          .get(`${process.env.REACT_APP_API_URL}/api/register`)
+          .then((response) => setWorkouts(response.data))
+          .catch((error) => console.log(error));
+      };
+
+      useEffect(() => {
+        getAllWorkouts();
+      }, [] );
   };
 
   return (
     <div className="FilterWorkouts">
-      <label>Select Exercises:</label>
+
+{workouts.map((workout) => {
+    return (
+        <div className='Tester'>
+<label>Select Exercises:</label>
       <select value={workoutname} onChange={handleSelect}>
-        <option value="All">{workoutname.nameOfWorkout}</option>
+        <option value="All">{workout.nameOfWorkout}</option>
         <option value="Chest">Chest</option>
         <option value="Triceps">Triceps</option>
         <option value="Biceps">Biceps</option>
@@ -24,6 +41,10 @@ function WorkoutFilter(props) {
         <option value="Legs">Legs</option>
         <option value="Abs">Abs</option>
       </select>
+      </div>
+    )
+})}
+      
     </div>
   );
 }
