c803d454b9b1f8138a5bb0f9e4babfcde6dedf2b
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-03-10 20:45:35 +0100

    Workoutselect list

--- src/components/WorkoutFilter.js
+++ src/components/WorkoutFilter.js
@@ -34,14 +34,7 @@ function WorkoutFilter(props) {
         <div className='Tester'>
 <label>Select Exercises:</label>
       <select value={workoutname} onChange={handleSelect}>
-        <option value="All">{workout.nameOfWorkout}</option>
-        <option value="Chest">Chest</option>
-        <option value="Triceps">Triceps</option>
-        <option value="Biceps">Biceps</option>
-        <option value="Shoulders">Shoulders</option>
-        <option value="Back">Back</option>
-        <option value="Legs">Legs</option>
-        <option value="Abs">Abs</option>
+        <option value={workout.nameOfWorkout}>{workout.nameOfWorkout}</option>
       </select>
       </div>
     )
