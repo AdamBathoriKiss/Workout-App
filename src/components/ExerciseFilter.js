067bb498b9a97742a0ead10b5af48a24129b80bc
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-03-11 02:06:30 +0100

    Workoutdetails

---
+++ src/components/ExerciseFilter.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+function ExerciseFilter(props) {
+  const [exercisname, setExerciseName] = useState('');
+  const [exercises, setExercises] = useState([]);
+
+    console.log(props);
+  const handleSelect = e => {
+    setExerciseName(e.target.value);
+    props.ExerciseFilter(e.target.value);
+    console.log('selected', e.target.value);
+
+  };
+
+  const getAllExercises = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/exercises`)
+      .then((response) => setExercises(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllExercises();
+  }, [] );
+
+ 
+  return (
+    <div className="FilterExercises">
+
+<div className='Tester'>
+<label>Select Exercises:</label>
+
+        <select value={workoutname} onChange={handleSelect}>
+        <option value="Select Exercise">Select Exercise</option>    
+        {exercises.map((exercise)=> <option value={exercise.nameOfExercise}>{exercise.nameOfExercise}</option>)}
+        </select>
+      </div>
+
+
+
+      
+    </div>
+  );
+}
+
+export default ExerciseFilter;
