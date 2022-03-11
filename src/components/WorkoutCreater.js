0df49fb0b9b1749adeeda913183f8334b6a17cae
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-03-11 07:31:00 +0100

    workout registration changed

--- src/components/WorkoutCreater.js
+++ src/components/WorkoutCreater.js
@@ -19,7 +19,7 @@ function WorkoutCreater(props) {
     const handleWorkout = (e) => {
         e.preventDefault();
     
-        const requestBody = { nameOfWorkout };
+        const requestBody = { nameOfWorkout, exercises, numberOfReps, sets };
      
         axios.post(`${process.env.REACT_APP_API_URL}/api/register`, requestBody)
         .then(() => navigate("/workouts"))
@@ -40,6 +40,14 @@ function WorkoutCreater(props) {
                     <label>Workout name</label>
                     <input type="text" className="form-control" placeholder="Chest day"  value={nameOfWorkout}
           onChange={handleName} />
+           <label>Exercise name</label>
+                    <input type="text" className="form-control" placeholder="Push up"  value={exercises}
+          onChange={handleName} />
+          <label>Number of Reps:</label>
+        <input type="number" className="form-control" placeholder="4" value={numReps} onChange={numberOfReps} />
+
+        <label>Number of Sets:</label>
+        <input type="number" className="form-control" placeholder="4" value={numSets} onChange={sets} />
                 </div>
          
                 <button type="submit" className="btn btn-primary btn-block" >Create</button>
